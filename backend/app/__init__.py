cef0ff3ae00f9ef31757b8be33d8d85049160b60
Russbell <rnorena@unal.edu.co>
2025-01-28 19:06:12 -0500

    fix(Deploy): :green_heart: fix secret key init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,7 +9,6 @@ def create_app():
     app = Flask(__name__)
 
     # Configuración básica
-    app.config['SECRET_KEY'] = Config.SECRET_KEY
     app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
 
