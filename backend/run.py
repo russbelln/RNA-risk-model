3490622d5503a4d26ab88111ef4cde8b1ce11326
Russbell <rnorena@unal.edu.co>
2025-01-29 00:55:15 -0500

    feat(DB): :construction: Add DB migrations

--- backend/run.py
+++ backend/run.py
@@ -2,6 +2,11 @@ import os
 from app import create_app, db
 from flask_migrate import Migrate
 
+from dotenv import load_dotenv
+
+# Cargar variables desde .env
+load_dotenv()
+
 app = create_app()
 migrate = Migrate(app, db)  # Agrega soporte para migraciones
 
@@ -9,4 +14,6 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Crea las tablas si no existen
         print("Base de datos inicializada en PostgreSQL.")
-    app.run(host="0.0.0.0", port=int(os.environ.get("BACKEND_PORT", 5000)))
+
+    port = int(os.environ.get("BACKEND_PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
