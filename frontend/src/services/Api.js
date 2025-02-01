08f758dd876e759cf277ea4cbb8a27d339638fc6
Russbell <rnorena@unal.edu.co>
2025-02-01 01:33:35 -0500

    feat(bug): :sparkles: CORS

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -3,6 +3,10 @@ import config from '../../config';
 
 const fetchData = axios.create({
   baseURL: `${config.apiBaseUrl}/`,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+  withCredentials: true,
 });
 
 export default fetchData;
