3cef61e56bb23a46a08f9e1fa282d939e2138079
Russbell <rnorena@unal.edu.co>
2025-02-11 16:11:48 -0500

    fix(bug): :bug: Fix autosigned certificate

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -3,6 +3,9 @@ import config from '../../config';
 
 const fetchData = axios.create({
   baseURL: `${config.apiBaseUrl}/`
+  httpsAgent: new (require('https').Agent)({  
+    rejectUnauthorized: false  // Permitir certificados autofirmados
+  })
 });
 
 export default fetchData;
